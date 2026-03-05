f28f85df9508d0913f7120d0d1f4b0834395b2f5
Christoph Breddin <cbreddin@web.de>
2026-03-05 22:16:02 +0100

    feat: initial project structure with player model and uuid

--- src/main/java/de/rebelmetal/schockenwebapp/SchockenWebAppApplication.java
+++ src/main/java/de/rebelmetal/schockenwebapp/SchockenWebAppApplication.java
@@ -1,13 +1,15 @@
 package de.rebelmetal.schockenwebapp;
 
+import de.rebelmetal.schockenwebapp.model.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchockenWebAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SchockenWebAppApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(SchockenWebAppApplication.class, args);
+
     }
+
 
 }
