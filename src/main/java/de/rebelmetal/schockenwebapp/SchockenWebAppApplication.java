4d118719fd76992c3da6bcea722a1e640bac7498
Christoph Breddin <cbreddin@web.de>
2026-03-05 19:59:51 +0100

    chore: initial spring boot setup with 3.5.11

---
+++ src/main/java/de/rebelmetal/schockenwebapp/SchockenWebAppApplication.java
@@ -0,0 +1,13 @@
+package de.rebelmetal.schockenwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchockenWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchockenWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/rebelmetal/schockenwebapp/SchockenWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rebelmetal.schockenwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchockenWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
